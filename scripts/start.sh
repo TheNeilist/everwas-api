c8103e2d5fcbad606513272069d5a0a060545133
TheNeilist <hireahoosier@yahoo.com>
2016-09-13 16:07:32 -0600

    explicit paths in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
-nohup java -jar ../target/everwas-api-0.0.1.jar server ../everwas-api.yml > log.txt 2> errors.txt < /dev/null &
+nohup java -jar /var/lib/jenkins/workspace/everwas-api/target/everwas-api-0.0.1.jar server /var/lib/jenkins/workspace/everwas-api/everwas-api.yml > log.txt 2> errors.txt < /dev/null &
 PID=$!
 echo $PID > /tmp/everwas-api-pid
