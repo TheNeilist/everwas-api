7f8eec2424c584524d3a7db8f16059aa9434fcb6
TheNeilist <hireahoosier@yahoo.com>
2016-10-21 00:46:56 -0400

    enable cors

--- src/main/java/com/theneilist/everwas/EverwasApplication.java
+++ src/main/java/com/theneilist/everwas/EverwasApplication.java
@@ -16,7 +16,12 @@ import io.dropwizard.jdbi.OptionalContainerFactory;
 import io.dropwizard.jdbi.args.OffsetDateTimeMapper;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.skife.jdbi.v2.DBI;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class EverwasApplication extends Application<EverwasConfiguration> {
 
@@ -40,6 +45,14 @@ public class EverwasApplication extends Application<EverwasConfiguration> {
 
     @Override
     public void run(EverwasConfiguration config, Environment environment) {
+
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
 
         final DBIFactory factory = new DBIFactory();
         DBI jdbi = factory.build(environment, config.getDataSourceFactory(), "postgresql");
