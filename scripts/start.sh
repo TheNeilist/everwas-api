36632aef391554a18fb2698e01a6759f7691409f
TheNeilist <hireahoosier@yahoo.com>
2016-10-28 11:37:08 -0400

    update start/stop scripts env var names

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 BUILD_ID=pleaseDontKillMe
 nohup java -jar /var/lib/jenkins/workspace/everwas-api/target/everwas-api-0.0.1.jar server /var/lib/jenkins/workspace/everwas-api/everwas-api.yml > log.txt 2> errors.txt < /dev/null &
-PID=$!
-echo $PID > /tmp/everwas-api-pid
-echo "started everwas-api PID $PID"
+EVERWAS_API_PID=$!
+echo $EVERWAS_API_PID > /tmp/everwas-api-pid
+echo "started everwas-api PID $EVERWAS_API_PID"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,8 +1,8 @@
 if [ -a /tmp/everwas-api-pid ]
         then
-                PID=$(cat /tmp/everwas-api-pid)
-		echo "Killing process $PID"
-                kill -9 $PID
+                EVERWAS_API_PID=$(cat /tmp/everwas-api-pid)
+		echo "Killing process $EVERWAS_API_PID"
+                kill -9 $EVERWAS_API_PID
                 rm -f /tmp/everwas-api-pid
 	else
 		echo "pid file does not exist"
